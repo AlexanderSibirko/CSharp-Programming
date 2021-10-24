d1daaaa86b11a0125a5f520f902c4053ae7cce56
Alexander <Alexandersibirko@yandex.ru>
2021-10-24 12:20:39 +0300

    solved dog question

--- DogProgram/Program.cs
+++ DogProgram/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+int personAspeed = 1;
+int personBspeed = 2;
+int dogspeed = 5;
+double distance = 10000;
+int nextfriend =2;
+double time = 0;
+
+while (distance > 10)
+{
+    if (nextfriend == 1)
+    {
+        time = distance/(personAspeed+dogspeed);
+        nextfriend = 2;
+    }
+    else
+    {
+        time = distance/(personBspeed+dogspeed);
+        nextfriend = 1;
+    }
+    distance = distance - (personAspeed+personBspeed) * time;
+    count++;
+}
+
+Console.WriteLine($"Собака перебежала от друга к другу {count} раз");
