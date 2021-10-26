f7fe514b09dfe42658ac48dded147a285649f66d
Alexander Sibirko <Alexandersibirko@yandex.ru>
2021-10-26 13:39:03 +0300

    added dog time spent and ditance runed

--- DogProgram/Program.cs
+++ DogProgram/Program.cs
@@ -2,9 +2,16 @@
 int personAspeed = 1;
 int personBspeed = 2;
 int dogspeed = 5;
-double distance = 10000;
 int nextfriend =2;
+// int time = 0;
+// int distance = 10000;
+// int totaltime = 0;
+// int totaldistance = 0; 
 double time = 0;
+double distance = 10000;
+double totaltime = 0;
+double totaldistance = 0; 
+
 
 while (distance > 10)
 {
@@ -18,8 +25,15 @@ while (distance > 10)
         time = distance/(personBspeed+dogspeed);
         nextfriend = 1;
     }
+    //Console.WriteLine(distance);
     distance = distance - (personAspeed+personBspeed) * time;
+    totaltime = totaltime + time;
+    totaldistance = totaldistance + (personAspeed+personBspeed) * time;
     count++;
 }
 
 Console.WriteLine($"Собака перебежала от друга к другу {count} раз");
+// Console.WriteLine($"Собака перебегала {totaltime} секунд");
+// Console.WriteLine($"Собака перебежала {totaldistance} метров");
+Console.WriteLine($"Собака перебегала {Math.Round(totaltime,4)} секунд");
+Console.WriteLine($"Собака перебежала {Math.Round(totaldistance,2)} метров");
